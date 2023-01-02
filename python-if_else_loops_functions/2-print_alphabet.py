e1c7a8ac81f8b6c66e0a08e702bb7a20841c4f07
root <geetanjalib@brightcomgroup.com>
2023-01-02 04:32:51 -0800

    2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'),ord('z')+1):
-    print("{ :c}".format(alpha_letters), end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    print("{:c}".format(alpha_letters), end="")
 
