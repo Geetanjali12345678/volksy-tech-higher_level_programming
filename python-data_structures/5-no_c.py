0615f46eecc426f100df87de3bedb607532e6b10
root <geetanjalib@brightcomgroup.com>
2023-01-05 21:39:57 -0800

    hi

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     copy = []
     for i in my_string:
-        if i !=  "C" and i != "c":
+        if i != "C" and i != "c":
             copy.append(i)
     return ''.join(copy)
