75f47caf78d76b6e05be3cb65fd4655cff746622
root <geetanjalib@brightcomgroup.com>
2023-01-07 09:45:00 -0800

    8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -6,4 +6,3 @@ def simple_delete(dic, k):
     else:
         return dic
 a_dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
-new_dict = simple_delete(a_dictionary, 'tr')
