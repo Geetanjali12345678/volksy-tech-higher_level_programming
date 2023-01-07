652e7cfc288b6407ef9c4c1d4558222df45cc8a0
root <geetanjalib@brightcomgroup.com>
2023-01-07 10:20:33 -0800

    12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+st = "DCCVII"
+d1 = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+sum =0
+
+for i in range(len(st)):
+    value = d1[st[i]]
+    if i+1 < len(st) and d1[st[i+1]] > value:
+        sum -= value
+    else:
+        sum += value
+print(sum)
