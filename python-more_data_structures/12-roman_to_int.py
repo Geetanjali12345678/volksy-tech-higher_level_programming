1b52a5499686a2520e8bcf8d23056a4843059669
root <geetanjalib@brightcomgroup.com>
2023-01-07 10:29:48 -0800

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,6 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string.isupper():
         return 0
-    # Dictionary for roman numerals
     r_dict = {
         "I": 1,
         "IV": 4,
