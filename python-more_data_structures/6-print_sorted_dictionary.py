3479d5416835ed00d4567122372099781048188c
root <geetanjalib@brightcomgroup.com>
2023-01-07 09:19:58 -0800

    6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(dic):
+    keys = list(dic.keys())
+    keys.sort()
+    for i in keys:
+        print('{}: {}'.format(i, dic[i]))
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
