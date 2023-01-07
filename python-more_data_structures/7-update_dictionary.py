24f8b130f18d4723ab990e78b8dd5b344fa19b37
root <geetanjalib@brightcomgroup.com>
2023-01-07 09:37:31 -0800

    7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,3 @@ def update_dictionary(dic, k, v):
     dic[k] = v
     return dic
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
