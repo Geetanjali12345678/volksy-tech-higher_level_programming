4fa9450c7d8c025b6b4bd82d6303da549fa64aed
root <geetanjalib@brightcomgroup.com>
2023-01-07 09:27:15 -0800

    6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(dic):
     keys.sort()
     for i in keys:
         print('{}: {}'.format(i, dic[i]))
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
