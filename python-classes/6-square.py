8d4fa77474926873f4ceb6b584769163b0d1b68c
root <geetanjalib@brightcomgroup.com>
2023-01-12 03:00:57 -0800

    x

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 class Square:
     """Defines a square"""
-
     def __init__(self, size=0, position=(0, 0)):
         """Initialises the data"""
         self.size = size
         self.position = position
-
 
     def area(self):
         """Returns current square area"""
