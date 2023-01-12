58e72bd363cbdd8d115e0bc3289fa05b03b749fb
root <geetanjalib@brightcomgroup.com>
2023-01-12 03:02:43 -0800

    x

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""square"""
+
+
 class Square:
     """Defines a square"""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialises the data"""
         self.size = size
