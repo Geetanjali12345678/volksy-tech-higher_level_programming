418b6ec6080dbf2e47e8a05545009399ebb2cbd1
root <geetanjalib@brightcomgroup.com>
2023-01-11 20:22:48 -0800

    x

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+'''square'''
+
+
 class Square:
+    '''init'''
+
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
