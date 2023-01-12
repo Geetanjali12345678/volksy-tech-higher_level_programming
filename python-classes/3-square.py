cb06a1ffd592bfa0ff8d2002db6042d19cfd6104
root <geetanjalib@brightcomgroup.com>
2023-01-11 20:25:24 -0800

    x

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,11 +10,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
-
         self.__size = size
-
-
     def area(self):
         ar = self.__size * self.__size
         return ar
