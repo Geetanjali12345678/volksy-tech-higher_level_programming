1d01c1fadbf9edc2a96601d978f5dda09c6eadf0
root <geetanjalib@brightcomgroup.com>
2023-01-18 03:19:39 -0800

    1

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Return an object by a JSON representation """
+import json
+
+
+def from_json_string(my_str):
+    """
+    Return an object by a JSON representation.
+
+    Args:
+        my_str: JSON representation
+
+    Raises:
+        Exception: when the string can't be decoded
