c0c048c35e347a010f1412531f6e5f5a29bfb97f
root <geetanjalib@brightcomgroup.com>
2023-01-18 02:56:49 -0800

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    """string"""
-with open(filename, "r", encoding="UTF-8") as f:
-    """jhhkk"""
+"""ddoc string"""
 
-    print(len(filename))
+
+
+def number_of_lines(filename=""):
+    """oooo"""
+
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
