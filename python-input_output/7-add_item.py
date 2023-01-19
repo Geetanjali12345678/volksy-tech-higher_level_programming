d1073e73a9aac001c6f9e9b44664e74caa0c639b
root <geetanjalib@brightcomgroup.com>
2023-01-19 00:57:27 -0800

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-
 fname = "add_item.json"
 
 ListyBoii = []
