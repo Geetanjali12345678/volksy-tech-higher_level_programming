70e634f7659afb37e49c7cb8d2f22af9022c6116
Geetanjali12345678 <geetanjalib@brightcomgroup.com>
2023-01-30 03:45:05 -0800

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- create table second_table
+-- create table second_table.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, “John”, 10);
 INSERT INTO second_table (id, name, score) VALUES (2, “Alex”, 3);
