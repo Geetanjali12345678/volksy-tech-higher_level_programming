1beaf681ac0bea02393d1996408e8f31208a3db5
root <geetanjalib@brightcomgroup.com>
2023-02-02 01:11:01 -0800

    10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,6 @@
--- create table
-SELECT cities.id,cities.name,states.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC;
+-- insert table
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+RIGHT JOIN tv_show_genres
+ON tv_show_genres.show_id=tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
