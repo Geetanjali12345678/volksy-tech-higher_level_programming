36d8bffd2fbf6ee0bd0eb1668d33f0152d3ac535
root <geetanjalib@brightcomgroup.com>
2023-02-12 05:45:53 -0800

    8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the database hbtn_0e_6_usa"""
+"""A script lists all State objects from the database hbtn_0e_6_usa"""
 
-import sqlalchemy
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
-                                                    sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    consul = session.query(State).order_by(State.id).consul()
-    if consul:
-        print("{}: {}".format(consul.id, consul.name))
-    else:
-        print('Nothing')
-
+    try:
+        print("{:}: ".format(session.query(State).order_by(
+            State.id).first().id), end="")
+        print(session.query(State).order_by(State.id).first().name)
+    except:
+        print("Nothing")
     session.close()
