0b2b83865f05c2d399dba6a465a939c88aedb609
Your Name <you@example.com>
2023-03-01 04:48:45 -0800

    x

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -2,16 +2,22 @@
 const request = require('request');
 const url = 'http://swapi.co/api/films/' + process.argv[2];
 let movieDict = {};
-const count = 0;
+let characterDict = {};
 let i = 0;
-const j = 0;
-request(url, function (
-  response, body) {
-  movieDict = JSON.parse(body.body);
-  for (i = 0; i < movieDict.characters.length; i++) {
-    request(movieDict.characters[i], function (request, body) {
-      characterDict = JSON.parse(body.body);
-      console.log(characterDict.name);
-    });
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    movieDict = JSON.parse(body);
+    for (i = 0; i < movieDict.characters.length; i++) {
+      request(movieDict.characters[i], function (error, request, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          characterDict = JSON.parse(body);
+          console.log(characterDict.name);
+        }
+      });
+    }
   }
 });
